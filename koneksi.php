958653c8bb902a80f15f6897e3d06f1f5e8fdd2b
LaBaskerville <alpinkino@gmail.com>
2026-07-14 18:34:05 +0700

    koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,10 @@
 <?php
 // Konfigurasi Database
-$host     = "localhost"; // Biasanya localhost
-$username = "root";      // Username default database
-$password = "";          // Password default (kosongkan jika tidak ada)
-$database = "dbmatcha";  // Nama database (lihat database.sql)
+$host     = "localhost"; 
+$username = "root"; 
+$password = "";          
+$database = "dbmatcha"; 
 
-// Membuat koneksi ke database
 $conn = new mysqli($host, $username, $password, $database);
 
 // Memeriksa apakah koneksi berhasil
@@ -13,6 +12,5 @@ if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
 
-// Set charset agar karakter (misal huruf non-ASCII) tersimpan dengan benar
 $conn->set_charset("utf8mb4");
 ?>
