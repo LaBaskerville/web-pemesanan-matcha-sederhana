e4c48329ffab0e220306a01c65c2d02e972b933e
LaBaskerville <alpinkino@gmail.com>
2026-07-14 18:35:37 +0700

    proses_pesan.php

--- proses_pesan.php
+++ proses_pesan.php
@@ -1,19 +1,5 @@
 <?php
-/**
- * proses_pesan.php
- * -------------------------------------------------------------
- * CATATAN PERUBAHAN:
- * Sebelumnya file ini langsung membuat 1 baris "pesanan" tanpa
- * memeriksa stok terlebih dahulu (bug), dan tanpa keranjang belanja
- * sama sekali (tidak sesuai kebutuhan "manajemen keranjang belanja").
- *
- * Sekarang alur pemesanan dari halaman utama diarahkan menjadi
- * "tambah ke keranjang" (lihat tambah_keranjang.php), lalu user
- * menyelesaikan pembelian lewat tombol Checkout pada bagian
- * "Keranjang Saya". File ini tetap dipertahankan agar form lama
- * (field: id_produk, jumlah_pesan) tetap berfungsi.
- * -------------------------------------------------------------
- */
+
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
@@ -23,7 +9,6 @@ include 'auth.php';
 require_login();
 
 if (isset($_POST['submit_pesan'])) {
-    // Petakan nama field lama ke field yang dipakai tambah_keranjang.php
     $_POST['id_produk'] = $_POST['id_produk'] ?? null;
     $_POST['jumlah']    = $_POST['jumlah_pesan'] ?? null;
 
